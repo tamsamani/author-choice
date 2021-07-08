860f6b59ff6afa2ce090ed0d190dfeba19ae0f22
tamsamani mohamed <tamsmoha@gmail.com>
2021-07-08 12:02:18 +0000

    upgrade Next and React

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ export default function Index() {
 	const [name, setName] = useState("MyApp");
 
 	useEffect(() => {
-		setName("OurApp");
+		setName("Authors App");
 	}, []);
 
 	return (
 		<div>
 			<h3>{name}</h3>
-			<p>The App where everything will show perfect.</p>
+			<p>The App where everything will show perfect. Comming Soon! exculsive for Authors.</p>
 		</div>
 	);
 }
